cc0e5c3068e5a0858228720dfd060b3df2889972
mrfhmd7 <mrf.hmd7@gmail.com>
2023-05-22 20:54:13 +0600

    active link added in to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
      return (
           <div className='bg-teal-800 text-white font-bold rounded-lg w-11/12 mx-auto h-14 flex justify-center items-center gap-x-16'>
-               <Link to='/'>Home</Link>
-               <Link to='meals'>Meals</Link>
+               <ActiveLink  to='/'>Home</ActiveLink>
+               <ActiveLink to='meals'>Meals</ActiveLink>
           </div>
      );
 };
